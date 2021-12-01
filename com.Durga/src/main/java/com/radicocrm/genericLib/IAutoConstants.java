11121ca4df6a0f7dee6546d4a581efe176685c8b
Sindhuja <Sindhuja@192.168.0.53>
2021-12-01 16:06:53 +0530

    My first commit

---
+++ com.Durga/src/main/java/com/radicocrm/genericLib/IAutoConstants.java
@@ -0,0 +1,13 @@
+package com.radicocrm.genericLib;
+
+public interface IAutoConstants {
+
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="C:\\Users\\Sindhuja\\eclipse-workspace\\com.Durga\\src\\main\\resources\\chromedriver.exe";
+	
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="C:\\Users\\Sindhuja\\eclipse-workspace\\com.Durga\\src\\main\\resources\\geckodriver.exe";
+	
+	String PROP_PATH="C:\\Users\\Sindhuja\\eclipse-workspace\\com.Durga\\src\\test\\resources\\config.properties";
+	
+}
